d546154953914080f8f1bd16e445a158b3ac234a
KSPOG <kratosmaximusosrs@gmail.com>
2025-08-15 21:24:12 +0200

    BankSeller
    Will obliterate your bank from Tradeable items, In other words, it will sell all tradeable items from your bank on the Grand Exchange

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/bankseller/BankSellerOverlay.java
@@ -0,0 +1,36 @@
+package net.runelite.client.plugins.microbot.bankseller;
+
+import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.ui.overlay.OverlayPanel;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.components.LineComponent;
+import net.runelite.client.ui.overlay.components.TitleComponent;
+
+import javax.inject.Inject;
+import java.awt.*;
+
+public class BankSellerOverlay extends OverlayPanel {
+
+    @Inject
+    BankSellerOverlay(BankSellerPlugin plugin) {
+        super(plugin);
+        setPosition(OverlayPosition.TOP_LEFT);
+        setNaughty();
+    }
+
+    @Override
+    public Dimension render(Graphics2D graphics) {
+        panelComponent.setPreferredSize(new Dimension(200, 300));
+        panelComponent.getChildren().add(TitleComponent.builder()
+                .text("Bank Seller")
+                .color(Color.GREEN)
+                .build());
+
+        panelComponent.getChildren().add(LineComponent.builder().build());
+
+        panelComponent.getChildren().add(LineComponent.builder()
+                .left(Microbot.status)
+                .build());
+        return super.render(graphics);
+    }
+}
